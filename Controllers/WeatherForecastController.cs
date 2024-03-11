f6ec25cec7c47463f25c260dc9f36e50de98d925
JayvonK <jaykhuth@gmail.com>
2024-03-11 11:13:30 -0700

    save

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using newWebApi.Services;
 
 // our root file. its a way for c# to organize our code
 // newWebApi = parent folder
@@ -22,25 +23,24 @@ public class WeatherForecastController : ControllerBase
     //this is a class
     private readonly ILogger<WeatherForecastController> _logger;
 
+    // Creating basically another variable or class and calling 
+    private readonly WeatherForecastService _data;
+
     // constructor = allows you to setup initial values or configurations for an object when it is created. This ensures that the object starts in a valid and consisten state
     // For example, a bike factory, when the frame and wheels are made, there is a station that takes care of putting them together. Thats the constructor
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    // We're injecting WeatherForecastService into the constructor, controller is able to get the necessary services from our services
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherForecastService data)
     {
         _logger = logger;
+        _data = data;
     }
 
     // This would be the READ in our CRUD functions
     [HttpGet(Name = "GetWeatherForecast")]
 
     // IEnumerable = a collection of objects that can be enumerated (iterated) one at a time
-    public IEnumerable<WeatherForecast> Get()
+    public IEnumerable<WeatherForecast> GetWeather()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return _data.GetWeather(Summaries);
     }
 }
